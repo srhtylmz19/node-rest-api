3deb1a6756556f0066e738b3bdb829a6d2e25a1d
Murat <murat@Murat-MacBook-Pro.local>
2021-02-10 21:49:20 +0200

    handle cors error

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+// HANDLE CORS !
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept , Authorization')
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET,  POST, PUT , PATCH, DELETE')
+        return res.status(200).json({})
+    }
+
+    next();
+})
+
+
 // API ROUTES to HANDLE REQUESTS !
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
